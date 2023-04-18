e660d6ef6d56a28217553b85fe745b716afa279b
Malak Mousa <malakmousa027@gmail.com>
2023-04-18 05:20:28 +0300

    Add show and hide password for the login page, and simple update in the login page

--- BeautyBookCustomerApp/BeautyBookCustomerApp/Services/Database.cs
+++ BeautyBookCustomerApp/BeautyBookCustomerApp/Services/Database.cs
@@ -13,7 +13,6 @@ namespace BeautyBookCustomerApp.Services
     {
         FirebaseClient firebaseClient = new FirebaseClient("https://beautybookapp-a44e5-default-rtdb.europe-west1.firebasedatabase.app/");
 
-
         public async Task<bool> SaveCustomerInfo(CustomerModel salon)
         {
             var data = await firebaseClient.Child(nameof(CustomerModel)).PostAsync(JsonConvert.SerializeObject(salon));
@@ -32,7 +31,7 @@ namespace BeautyBookCustomerApp.Services
 
             foreach (var item in salonInfo)
             {
-                if (item.Object.username == Username && item.Object.password == Password)
+                if (item.Object.Username == Username && item.Object.Password == Password)
                 {
                     return true;
                 }
